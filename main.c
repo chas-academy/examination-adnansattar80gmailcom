b3a7350c8e4e19fc9c0c1a0cd4129c8c19b0fc58
adnansattar80@gmail.com <adnansattar80@gmail.com>
2025-08-06 21:07:53 +0200

    Fourth try

--- main.c
+++ main.c
@@ -1,59 +1,40 @@
 #include <stdio.h>
 #include <ctype.h>
 
-void calculate_averages(int scores[5][13], double averages[5], char names[5][11]) {
+int main() {
+    char names[5][11];
+    int scores[5][13];
+    double averages[5];
+
+    // Läs in namn och poäng
+    for (int i = 0; i < 5; i++) {
+        scanf("%10s", names[i]);
+        for (int j = 0; j < 13; j++) {
+            scanf("%d", &scores[i][j]);
+        }
+        names[i][0] = toupper(names[i][0]);  // Gör första bokstaven versal
+    }
+
+    // Räkna ut snitt för varje elev
+    double total = 0.0;
     for (int i = 0; i < 5; i++) {
         int sum = 0;
         for (int j = 0; j < 13; j++) {
             sum += scores[i][j];
         }
         averages[i] = sum / 13.0;
-        printf("%s %.2f\n", names[i], averages[i]);
-    }
-}
-
-void find_highest_average(double averages[5], char names[5][11]) {
-    double highest = averages[0];
-    int index = 0;
-    for (int i = 1; i < 5; i++) {
-        if (averages[i] > highest) {
-            highest = averages[i];
-            index = i;
-        }
-    }
-    printf("%.2f %s\n", highest, names[index]);
-}
-
-void print_below_group_average(double averages[5], char names[5][11]) {
-    double total = 0.0;
-    for (int i = 0; i < 5; i++) {
         total += averages[i];
     }
+
+    // Gruppsnitt
     double group_average = total / 5.0;
 
+    // Skriv ut endast de elever som ligger under gruppsnittet
     for (int i = 0; i < 5; i++) {
         if (averages[i] < group_average) {
-            printf("%s %.2f\n", names[i], averages[i]);
+            printf("%s\n", names[i]);
         }
     }
-}
-
-int main() {
-    char names[5][11];
-    int scores[5][13];
-
-    for (int i = 0; i < 5; i++) {
-        scanf("%10s", names[i]);
-        for (int j = 0; j < 13; j++) {
-            scanf("%d", &scores[i][j]);
-        }
-        names[i][0] = toupper(names[i][0]);
-    }
-
-    double averages[5];
-    calculate_averages(scores, averages, names);
-    find_highest_average(averages, names);
-    print_below_group_average(averages, names);
 
     return 0;
 }
