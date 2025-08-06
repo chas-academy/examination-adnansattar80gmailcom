cf4b388f1ed9df3488ce26c103bb41de9c308479
adnansattar80@gmail.com <adnansattar80@gmail.com>
2025-08-06 21:02:05 +0200

    Third try

--- main.c
+++ main.c
@@ -8,7 +8,7 @@ void calculate_averages(int scores[5][13], double averages[5], char names[5][11]
             sum += scores[i][j];
         }
         averages[i] = sum / 13.0;
-        printf("Average score for %s: %.2f\n", names[i], averages[i]);
+        printf("%s %.2f\n", names[i], averages[i]);
     }
 }
 
@@ -21,55 +21,38 @@ void find_highest_average(double averages[5], char names[5][11]) {
             index = i;
         }
     }
-    printf("\nHighest average score is %.2f for student %s\n", highest, names[index]);
+    printf("%.2f %s\n", highest, names[index]);
 }
 
 void print_below_group_average(double averages[5], char names[5][11]) {
-    // Step 2: Calculate group average
     double total = 0.0;
     for (int i = 0; i < 5; i++) {
         total += averages[i];
     }
     double group_average = total / 5.0;
-    
-    
-    printf("Group average score: %.2f\n", group_average);
-    printf("\nAverage score below group average:\n");
-    
+
     for (int i = 0; i < 5; i++) {
         if (averages[i] < group_average) {
-            printf("%s (%.2f)\n", names[i], averages[i]);
+            printf("%s %.2f\n", names[i], averages[i]);
         }
     }
 }
 
 int main() {
-    char names[5][11];       // 5 students, max 10 characters + null terminator
-    int scores[5][13];       // 5 students, 13 test scores
+    char names[5][11];
+    int scores[5][13];
 
-    printf("Enter name and scores for each student (Name p1 p2 p3 p4 ... p13):\n");
-
-    // Read names and scores
     for (int i = 0; i < 5; i++) {
         scanf("%10s", names[i]);
         for (int j = 0; j < 13; j++) {
             scanf("%d", &scores[i][j]);
         }
-    }
-
-    // Capitalize the first letter of each name
-    for (int i = 0; i < 5; i++) {
         names[i][0] = toupper(names[i][0]);
     }
 
-
-
-    printf("\nAverage scores:\n");
     double averages[5];
     calculate_averages(scores, averages, names);
-
     find_highest_average(averages, names);
-
     print_below_group_average(averages, names);
 
     return 0;
