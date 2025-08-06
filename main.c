0e03e45fe43f1d2f931296b773b5aab660df497b
adnansattar80@gmail.com <adnansattar80@gmail.com>
2025-08-06 19:01:52 +0200

    Second try

--- main.c
+++ main.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <ctype.h>
 
-void calculate_averages(int scores[5][13], double averages[5]) {
+void calculate_averages(int scores[5][13], double averages[5], char names[5][11]) {
     for (int i = 0; i < 5; i++) {
         int sum = 0;
         for (int j = 0; j < 13; j++) {
             sum += scores[i][j];
         }
         averages[i] = sum / 13.0;
-        printf("Average score for student %d: %.2f\n", i + 1, averages[i]);
+        printf("Average score for %s: %.2f\n", names[i], averages[i]);
     }
 }
 
@@ -21,7 +21,7 @@ void find_highest_average(double averages[5], char names[5][11]) {
             index = i;
         }
     }
-    printf("Highest average score is %.2f for student %s\n", highest, names[index]);
+    printf("\nHighest average score is %.2f for student %s\n", highest, names[index]);
 }
 
 void print_below_group_average(double averages[5], char names[5][11]) {
@@ -32,8 +32,9 @@ void print_below_group_average(double averages[5], char names[5][11]) {
     }
     double group_average = total / 5.0;
     
-    printf("\nAverage score below group average:\n");
+    
     printf("Group average score: %.2f\n", group_average);
+    printf("\nAverage score below group average:\n");
     
     for (int i = 0; i < 5; i++) {
         if (averages[i] < group_average) {
@@ -46,7 +47,7 @@ int main() {
     char names[5][11];       // 5 students, max 10 characters + null terminator
     int scores[5][13];       // 5 students, 13 test scores
 
-    printf("Enter the name and 13 scores for each student (Name s1 s2 ... s13):\n");
+    printf("Enter name and scores for each student (Name p1 p2 p3 p4 ... p13):\n");
 
     // Read names and scores
     for (int i = 0; i < 5; i++) {
@@ -60,20 +61,16 @@ int main() {
     for (int i = 0; i < 5; i++) {
         names[i][0] = toupper(names[i][0]);
     }
-    for (int i = 0; i < 5; i++) {
-        printf("%s\n", names[i]);
-    }
+
+
 
     printf("\nAverage scores:\n");
     double averages[5];
-    calculate_averages(scores, averages);
+    calculate_averages(scores, averages, names);
 
     find_highest_average(averages, names);
 
-    // ➕ Step 2: Print students below group average
     print_below_group_average(averages, names);
-
-
 
     return 0;
 }
